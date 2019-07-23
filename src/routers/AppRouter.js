35cd760b4a9ecc2aa3f0b9e75c9e5f6475772ec6
Phillip <patransd@berkeley.edu>
2019-07-23 11:33:54 -0700

    Add private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import LoginPage from '../components/LoginPage.js';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage.js';
 import AddExpensePage from '../components/AddExpensePage.js';
 import EditExpensePage from '../components/EditExpensePage.js';
 import HelpPage from '../components/HelpPage.js';
 import NotFoundPage from '../components/NotFoundPage.js';
-import Header from '../components/Header.js';
+import PrivateRoute from './PrivateRoute';
+
+export const history = createBrowserHistory();
 
 const AppRouter = () => (
-    <BrowserRouter>
+    <Router history={history}>
         <div>
-            <Header />
                 <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-                <Route path="/create" component={AddExpensePage}/>
-                <Route path="/edit/:id" component={EditExpensePage}/>
+                <Route path="/" component={LoginPage} exact={true}/>
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
+                <PrivateRoute path="/create" component={AddExpensePage}/>
+                <PrivateRoute path="/edit/:id" component={EditExpensePage}/>
                 <Route path="/help" component={HelpPage}/>
                 <Route component={NotFoundPage}/>
             </Switch>
         </div>
 
 
-    </BrowserRouter>
+    </Router>
 );
 
 export default AppRouter;
--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,11 +1,6 @@
 import expensesReducer from '../../reducers/expenses';
 import expenses from '../fixtures/expenses';
 
-test('should set default state', () => {
-    const state = expensesReducer(undefined, { type: '@@INIT'});
-    expect(state).toEqual([]);
-});
-
 test('should remove expense by id', () => {
     const action = {
         type: 'REMOVE_EXPENSE',
