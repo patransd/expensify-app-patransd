768f7b52a9b63195fd52a490f4244a7576475696
Phillip <patransd@berkeley.edu>
2019-07-25 10:54:10 -0700

    Add Expensify styles

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,22 +4,31 @@ import { connect } from 'react-redux';
 import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => (
-    <div>
-        {
-            props.expenses.length === 0 ? (
-                <p> No expenses</p>
-            ) : (
-                props.expenses.map((expense) => {
-                    return <ExpenseListItem key={expense.id} {...expense}/>
-                })
-            )
-        }
+    <div className="content-container">
+        <div className="list-header">
+            <div className="show-for-mobile">Expenses</div>
+            <div className="show-for-other">Expense</div>
+            <div className="show-for-other">Amount</div>
+        </div>
+        <div className="list-body">
+            {
+                props.expenses.length === 0 ? (
+                    <div className="list-item--message">
+                        <span>No expenses</span>
+                    </div>
+                ) : (
+                        props.expenses.map((expense) => {
+                            return <ExpenseListItem key={expense.id} {...expense} />
+                        })
+                    )
+            }
+        </div>
     </div>
 );
 
 const mapStateToProps = (state) => {
     return {
-        expenses: selectExpenses(state.expenses, state.filters)    
+        expenses: selectExpenses(state.expenses, state.filters)
     };
 };
 
